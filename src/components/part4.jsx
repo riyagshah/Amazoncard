4395b326885b41ee713359048f0edaa18823f929
riyagshah <riyagshah4@gmail.com>
2022-05-22 00:55:00 +0530

    message

--- src/components/part4.jsx
+++ src/components/part4.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import styles from "../components/cardstyle.module.css"
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleRight} from "@fortawesome/free-solid-svg-icons";
 
 let Part4=({arrow,devices})=>{
     
     return(
         <div className={styles.part4}>
             <p>{devices}</p>
-            <img  style={{ width:"50px",height:"50px",marginLeft:"60%",marginTop:"3%"}} src={arrow} ></img>
+            {/* <img  style={{ width:"50px",height:"50px",marginLeft:"60%",marginTop:"3%"}} src={arrow} ></img> */}
+            <FontAwesomeIcon style={{ width:"50px",height:"50px",marginLeft:"60%",marginTop:"3%"}} icon={faArrowAltCircleRight}></FontAwesomeIcon>
         </div>
     )
 }
